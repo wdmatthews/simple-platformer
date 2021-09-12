2d1d36da0223dfd2540e3a8a5592b3509a4a5d71
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2021-09-12 08:14:52 -0500

    Made hazards damage characters after their invincibility ends

--- Assets/_Project/Scripts/Environment/Hazard.cs
+++ Assets/_Project/Scripts/Environment/Hazard.cs
@@ -9,18 +9,31 @@ namespace Project.Environment
         [SerializeField] protected string _characterLayerName = "Character";
 
         protected int _characterLayer = 0;
+        protected Character _characterInTrigger = null;
 
         protected void Awake()
         {
             _characterLayer = LayerMask.NameToLayer(_characterLayerName);
         }
 
+        protected void Update()
+        {
+            if (_characterInTrigger) DamageCharacter(_characterInTrigger);
+        }
+
         protected void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.layer == _characterLayer)
             {
-                DamageCharacter(collision.GetComponent<Character>());
+                Character character = collision.GetComponent<Character>();
+                _characterInTrigger = character;
+                DamageCharacter(character);
             }
+        }
+
+        protected void OnTriggerExit2D(Collider2D collision)
+        {
+            _characterInTrigger = null;
         }
 
         public void DamageCharacter(Character character)
